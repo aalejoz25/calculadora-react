8a4e4957d6e6581bd40ad1bc5f13b2155a1e8133
aalejoz25 <alvaroalejo25@gmail.com>
2023-11-20 17:33:36 -0500

    se termina calculadora en react

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,76 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Button from "./components/Button";
+import Screen from "./components/Screen";
+import ClearButton from "./components/ClearButton";
 import "./App.css";
+import { evaluate } from "mathjs";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  const addInput = (val) => {
+    if (
+      input.length == 0 &&
+      (val == "+" || val == "-" || val == "*" || val == "/" || val == ".")
+    ) {
+      alert("Ingrese primero un numero");
+    } else if (
+      input.charAt(input.length - 1) == "+" ||
+      input.charAt(input.length - 1) == "-" ||
+      input.charAt(input.length - 1) == "*" ||
+      input.charAt(input.length - 1) == "/" ||
+      input.charAt(input.length - 1) == "."
+    ) {
+      alert("No insgrese dos operadores seguidos");
+    } else {
+      setInput(input + val);
+    }
+  };
+  const calcResult = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert("Porfavor ingrese valores");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="logo-contenedor">
+        <img className="logo" src={reactLogo} alt="App logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="contenedor-calculadora">
+        <Screen input={input}></Screen>
+        <div className="fila">
+          <Button manejarClic={addInput}>1</Button>
+          <Button manejarClic={addInput}>2</Button>
+          <Button manejarClic={addInput}>3</Button>
+          <Button manejarClic={addInput}>+</Button>
+        </div>
+        <div className="fila">
+          <Button manejarClic={addInput}>4</Button>
+          <Button manejarClic={addInput}>5</Button>
+          <Button manejarClic={addInput}>6</Button>
+          <Button manejarClic={addInput}>-</Button>
+        </div>
+        <div className="fila">
+          <Button manejarClic={addInput}>7</Button>
+          <Button manejarClic={addInput}>8</Button>
+          <Button manejarClic={addInput}>9</Button>
+          <Button manejarClic={addInput}>*</Button>
+        </div>
+        <div className="fila">
+          <Button manejarClic={calcResult}>=</Button>
+          <Button manejarClic={addInput}>0</Button>
+          <Button manejarClic={addInput}>.</Button>
+          <Button manejarClic={addInput}>/</Button>
+        </div>
+        <div className="fila">
+          <ClearButton handleClear={() => setInput("")}>Clear</ClearButton>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/Screen.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./../stylesheets/Screen.css";
+
+const Screen = ({ input }) => <div className="screen">{input}</div>;
+
+export default Screen;
